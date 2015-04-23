be5f02162a6db9af23704c81c09084b55a1c028e
ajguyot <ajguyot@icloud.com>
2015-04-22 22:25:46 -0700

    Recompiled back to Java 7...

--- FinalProject/src/model/Bow.java
+++ FinalProject/src/model/Bow.java
@@ -2,7 +2,7 @@ package model;
 //335 Final Project - Red Squadron
 //Authors: Alex Guyot and John Oney
 
-public class Bow extends Weapon{
+public class Bow extends Weapon {
 
 	public Bow(int cost, String name, int range, int might, int accuracy,int critical) {
 		super(cost, name, range, might, accuracy, critical);
--- FinalProject/src/model/Equipment.java
+++ FinalProject/src/model/Equipment.java
@@ -2,7 +2,7 @@ package model;
 // 335 Final Project - Red Squadron
 // Authors: Alex Guyot and John Oney
 
-public abstract class Equipment extends Item{
+public abstract class Equipment extends Item {
 	// private variables
 	private int cost;
 	private String name;
